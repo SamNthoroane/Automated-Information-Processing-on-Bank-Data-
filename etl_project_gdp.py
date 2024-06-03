ddfb037a36417e727fd61ed2f28d61872052e5d3
SamNthoroane <sam.nthoroane@gmail.com>
2024-06-03 19:02:01 +0200

    Transform-method

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -1,5 +1,29 @@
 # Code for ETL operations on Country-GDP data
 
 # Importing the required libraries
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+from datetime import datetime 
 
+def extract(url, table_attribs):
+#Extracts information from website
 
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
+
+    for row in rows:
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Country": col[0].a.contents[0],
+                             "GDP_USD_millions": col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+
+    return df
