66595b83070afe7c42e5e1c7db05704c7f05e271
SamNthoroane <sam.nthoroane@gmail.com>
2024-06-03 21:10:39 +0200

    Querying data

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -92,3 +92,11 @@ log_progress('SQL Connection initiated.')
 load_to_db(df, sql_connection, table_name)
 
 log_progress('Data loaded to Database as table. Running the query')
+
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()
