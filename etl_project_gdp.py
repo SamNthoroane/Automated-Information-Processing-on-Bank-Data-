cfc210550c0a2c3390ef2a883391e8ab47ee2896
SamNthoroane <sam.nthoroane@gmail.com>
2024-06-03 20:41:47 +0200

    Function calls in order

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -65,4 +65,30 @@ def log_progress(message):
         f.write(timestamp + ' : ' + message + '\n')    
 #Definition of required entities
 
- 
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attribs = ["Country", "GDP_USD_millions"]
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = './Countries_by_GDP.csv'
+
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, csv_path)
+
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect('World_Economies.db')
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as table. Running the query')
