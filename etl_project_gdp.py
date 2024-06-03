cc42769c176ab3cd505d25a7a744b005472ba231
SamNthoroane <sam.nthoroane@gmail.com>
2024-06-03 20:32:11 +0200

    Loading to csv and db

--- etl_project_gdp.py
+++ etl_project_gdp.py
@@ -40,3 +40,11 @@ def transform(df):
     df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
     
     return df
+
+def load_to_csv(df, csv_path):
+    # This function saves the final dataframe as a `CSV` file in the provided path. Function returns nothing.
+    df.to_csv(csv_path)
+
+def load_to_db(df, sql_connection, table_name):
+    # This function saves the final dataframe to as a database table with the provided name. Function returns nothing.
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
